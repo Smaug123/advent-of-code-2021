bd01449610d18d67dd6506387c98bd355e621e89
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-12-01 22:44:02 +0000

    Black-box the day 1 benchmark

--- day_1/benches/day_1.rs
+++ day_1/benches/day_1.rs
@@ -1,16 +1,16 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use day_1::day_1::{input, part_1, part_2};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let input = input();
     c.bench_function("day 1 part 1", |b| {
         b.iter(|| {
-            part_1(&mut input.iter().copied());
+            black_box(part_1(&mut input.iter().copied()));
         })
     });
     c.bench_function("day 1 part 2", |b| {
         b.iter(|| {
-            part_2(&input);
+            black_box(part_2(&input));
         })
     });
 }
