5527535c2e204cf79a250e9bbede3b4877a223bf
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-12-02 18:19:12 +0000

    Day 2 in Mathematica

--- day_2/day_1.m
+++
@@ -1,5 +0,0 @@
-ints = ReadList[StringJoin[NotebookDirectory[], "/input.txt"], "Number"];
-
-part1 = Tr@Clip[Differences@#, {0, 1}] &;
-
-part2 = Tr@Boole[Less @@@ Partition[#, 4, 1][[All, {1, 4}]]] &;
