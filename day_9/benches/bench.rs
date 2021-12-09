2e4d9d8078febdc25b7cef033e0c1724d70733d0
Patrick Stevens <patrick+github@patrickstevens.co.uk>
2021-12-09 19:10:30 +0000

    Day 9 in Rust (#7)

---
+++ day_9/benches/bench.rs
@@ -0,0 +1,19 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use day_9::day_9::{input, part_1, part_2};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let input = input();
+    c.bench_function("day 9 part 1", |b| {
+        b.iter(|| {
+            black_box(part_1(&input));
+        })
+    });
+    c.bench_function("day 9 part 2", |b| {
+        b.iter(|| {
+            black_box(part_2(&input));
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
