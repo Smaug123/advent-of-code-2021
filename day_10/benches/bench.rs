b5de0d74d53ef274d9b0c8e67dd0cdc09cdf55a5
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-12-10 05:53:59 +0000

    Day 10 in Rust

---
+++ day_10/benches/bench.rs
@@ -0,0 +1,19 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use day_10::day_10::{input, part_1, part_2};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let input = input();
+    c.bench_function("day 10 part 1", |b| {
+        b.iter(|| {
+            black_box(part_1(&input));
+        })
+    });
+    c.bench_function("day 10 part 2", |b| {
+        b.iter(|| {
+            black_box(part_2(&input));
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ day_10/src/lib.rs
@@ -0,0 +1,123 @@
+pub mod day_10 {
+
+    pub(crate) fn parse(s: &str) -> Vec<&str> {
+        s.trim().split('\n').collect()
+    }
+
+    pub fn input() -> Vec<&'static str> {
+        parse(include_str!("../input.txt"))
+    }
+
+    fn closing(c: char) -> Option<char> {
+        if c == '[' {
+            Some(']')
+        } else if c == '{' {
+            Some('}')
+        } else if c == '(' {
+            Some(')')
+        } else if c == '<' {
+            Some('>')
+        } else {
+            None
+        }
+    }
+
+    /// Return Ok(first illegal char), or Err(ending) for an incomplete string.
+    fn syntax_error(s: &str) -> Result<char, Vec<char>> {
+        let mut stack = Vec::new();
+        for c in s.chars() {
+            match closing(c) {
+                Some(closing) => {
+                    stack.push(closing);
+                }
+                None => match stack.pop() {
+                    None => {
+                        return Ok(c);
+                    }
+                    Some(popped) => {
+                        if popped != c {
+                            return Ok(c);
+                        }
+                    }
+                },
+            }
+        }
+        stack.reverse();
+        Err(stack)
+    }
+
+    pub fn part_1(data: &[&str]) -> u32 {
+        data.iter()
+            .map(|line| match syntax_error(line) {
+                Ok(')') => 3,
+                Ok(']') => 57,
+                Ok('}') => 1197,
+                Ok('>') => 25137,
+                Ok(_) => panic!("Oh no"),
+                Err(_) => 0,
+            })
+            .sum()
+    }
+
+    pub fn part_2(data: &[&str]) -> u64 {
+        let mut scores: Vec<u64> = data
+            .iter()
+            .filter_map(|line| match syntax_error(line) {
+                Err(completion) => Some(
+                    completion
+                        .iter()
+                        .map(|c| match c {
+                            ')' => 1,
+                            ']' => 2,
+                            '}' => 3,
+                            '>' => 4,
+                            _ => {
+                                panic!("Unexpected");
+                            }
+                        })
+                        .fold(0, |state, new_val| state * 5 + new_val),
+                ),
+                Ok(_) => None,
+            })
+            .collect();
+        scores.sort_unstable();
+        scores[scores.len() / 2]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::day_10::*;
+
+    static TEST_INPUT: &str = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+    #[test]
+    fn part1_known() {
+        let data = parse(TEST_INPUT);
+
+        assert_eq!(part_1(&data), 26397);
+    }
+
+    #[test]
+    fn part2_known() {
+        let data = parse(TEST_INPUT);
+
+        assert_eq!(part_2(&data), 288957);
+    }
+
+    #[test]
+    fn test_day_10() {
+        let input = input();
+        assert_eq!(part_1(&input), 392043);
+        assert_eq!(part_2(&input), 1605968119);
+    }
+}
