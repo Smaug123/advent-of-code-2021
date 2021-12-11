54933fc420f424b3d9694b1d68859f45b2be3294
Patrick Stevens <patrick+github@patrickstevens.co.uk>
2021-12-11 08:54:45 +0000

    Day 11 in Rust (#11)

---
+++ day_11/src/main.rs
@@ -0,0 +1,7 @@
+use day_11::day_11::{input, part_1, part_2};
+
+fn main() {
+    let input = input();
+    println!("part 1 => {}", part_1(&input));
+    println!("part 2 => {}", part_2(&input));
+}
