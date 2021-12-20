64226237959f609a8a9a4994b6b02aa6a6de623f
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-12-20 12:56:49 +0000

    Remove some Mathematica nonanswers

--- day_3/day_3.m
+++
@@ -1,7 +0,0 @@
-(* ::Package:: *)
-
-nums=Import[StringJoin[NotebookDirectory[], "/input.txt"],"Table"];
-
-part1 = 1##&@@Tr[1##&@@@(#/."up"->-"down")]&;
-
-part2=1##&@@Rest@Fold[Function[{state,elt},Switch[elt[[1]],"forward",state+{0,elt[[2]],state[[1]] elt[[2]]},"down",MapAt[elt[[2]]+#&,state,1],"up",MapAt[#-elt[[2]]&,state,1]]],{0,0,0},#]&;
